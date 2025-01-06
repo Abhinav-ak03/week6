f10c24987b6d15ee0281ddc3f486739320159f34
Abhinav-ak03 <abhinav.ak03@gmail.com>
2025-01-06 19:14:59 +0530

    user schema added

---
+++ models/userSchema.js
@@ -0,0 +1,43 @@
+const mongoose=require("mongoose");
+const {Schema}= mongoose;
+
+const userSchema = new Schema ({
+    name: {
+        type:String,
+        required:true
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:false,
+        unique:false,
+        sparse:true,
+        default:null
+    },
+    googleId:{
+        type:String,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:false,
+    },
+    isBlocked:{
+        type:Boolean,
+        default:false
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+
+})
+
+
+const user = mongoose.model("user",userSchema)
+
+module.exports = user;
